6258d58f0d14e5b838e58067c1fe0c088f4e0af7
aditya <thisisadityachebrolu@gmail.com>
2021-05-16 01:57:10 +0530

    again

--- src/routers/scannerRouter.js
+++ src/routers/scannerRouter.js
@@ -27,7 +27,7 @@ router.post('/scannerlogin',async(req,res)=>{
         const admin=await Admin.findOne({'scanners.number':req.body.number,'places.pincode':req.body.pincode}) 
         if(admin)
         {
-            const place=await Admin.findOne({'places.pincode':req.body.pincode})
+            const place=admin.places.find(place=>place.pincode===req.body.pincode)
             return res.send(place)
         }
         throw new Error()
